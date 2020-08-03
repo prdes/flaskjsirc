e87a510d4addbd4a8f5d4e18ed272c04bf1e20cd
mogad0n <melodon@tuta.io>
2020-08-03 16:52:50 +0530

    initial commit

---
+++ hello.js
@@ -0,0 +1,12 @@
+let express = require('express')
+let app = express()
+let port = 3000
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http:/localhost:${port}`)
+})
