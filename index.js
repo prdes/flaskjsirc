4102a8781c21c31d0a59f9e4eb4674cc96316aea
mogad0n <melodon@tuta.io>
2020-08-05 03:01:33 +0530

    implement basic registration and verification bots

--- index.js
+++ index.js
@@ -34,25 +34,43 @@ app.post('/register', function (req, res) {
             next();
         }); 
     });
-
-    bot.on('registered', function ({ client }) {
-        client.say('nickserv', `register ${req.body.password} ${req.body.email}`);
-    });
+    
+   
 
     bot.connect({
         host: 'irc.liberta.casa',
         port: 6697,
+        tls: true,
         nick: req.body.username,
     });
-
-    console.log("Client Connected");
-    console.log(bot)
-    // const ircClient = new Client({ encoding: 'utf-8' });
-    // const ircClient.use(); // Use middleware to registr
+ 
+    bot.on('registered', function ({ client }) {
+        bot.say('nickserv', `register ${req.body.password} ${req.body.email}`);
+    });
   });
 
 
 
 app.get('/verify', function (req, res) {
 res.render('verify', { title: 'Verify', message: 'Verification' })
+})
+
+app.post('/verify', function (req, res) {
+    let bot = new IRC.Client();
+
+    // bot.use(function (client, rawEvents, parsedEvents) { 
+    //     parsedEvents.use(function (command, event, client, next) {
+    //         console.log(event);
+    //         next();
+    //     }); 
+    // });
+    bot.connect({
+        host: 'irc.liberta.casa',
+        port: 6697,
+        tls: true,
+        nick: req.body.username,
+    });
+    bot.on('registered', function ({ client }) {
+        bot.say('nickserv', `verify ${req.body.username} ${req.body.verif-code}`);
+    });
 })
--- ircVerify.js
+++
@@ -1,7 +0,0 @@
-var bot = new IRC.Client();
-bot.connect({
-	host: 'localhost',
-	port: 6667,
-	nick: 'UseUserNickHere'
-});
-
